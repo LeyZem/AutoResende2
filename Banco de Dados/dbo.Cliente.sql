8671180b4f22cda1f15331d00f85cd4b4fdcb6fe
LeyZem <tdb_mc@hotmail.com>
2015-05-31 16:23:40 -0300

    Banco de Dados atualizado

---
+++ Banco de Dados/dbo.Cliente.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Cliente] (
+    [idCliente]      INT          NOT NULL,
+    [Nome]           VARCHAR (50) NOT NULL,
+    [CPF]            VARCHAR (50) NOT NULL,
+    [RG]             VARCHAR (50) NOT NULL,
+    [OrgaoExpedidor] VARCHAR (50) NOT NULL,
+    [DataNascimento] DATE         NOT NULL,
+    [Logradouro]     VARCHAR (50) NOT NULL,
+    [Bairro]         VARCHAR (50) NOT NULL,
+    [Cidade]         VARCHAR (50) NOT NULL,
+    [UF]             VARCHAR (50) NOT NULL,
+    [CEP]            VARCHAR (50) NOT NULL,
+    [TelRes]         VARCHAR (50) NULL,
+    [TelCel]         VARCHAR (50) NULL,
+    [email]          VARCHAR (50) NULL,
+    [Comentario] VARCHAR(50) NULL, 
+    CONSTRAINT [PK_Cliente] PRIMARY KEY CLUSTERED ([idCliente] ASC)
+);
+
