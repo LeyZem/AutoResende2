375d64b00d8d59e233e5dd78bb2100a9bc36e0d0
Cristimasiero <cristimasiero@hotmail.com>
2015-05-31 19:40:18 -0300

    Ajustes Telas 2

--- AutoResende.View/Cadastros/frmCadastroOrdemServico.Designer.cs
+++ AutoResende.View/Cadastros/frmCadastroOrdemServico.Designer.cs
@@ -29,45 +29,280 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCadastroOrdemServico));
-            this.btnFechar = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picimg_cliente = new System.Windows.Forms.PictureBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbldescrição = new System.Windows.Forms.Label();
+            this.lblinscestadual = new System.Windows.Forms.Label();
+            this.lblfantasia = new System.Windows.Forms.Label();
+            this.Btn_Apagar = new System.Windows.Forms.Button();
+            this.Btn_Gravar = new System.Windows.Forms.Button();
+            this.lblfuncionários = new System.Windows.Forms.Label();
+            this.lblCódigo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_Codigo = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picimg_cliente)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnFechar
+            // pictureBox3
             // 
-            this.btnFechar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnFechar.BackColor = System.Drawing.Color.Transparent;
-            this.btnFechar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnFechar.BackgroundImage")));
-            this.btnFechar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnFechar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnFechar.FlatAppearance.BorderSize = 0;
-            this.btnFechar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.btnFechar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnFechar.ForeColor = System.Drawing.Color.Transparent;
-            this.btnFechar.Location = new System.Drawing.Point(938, 12);
-            this.btnFechar.Name = "btnFechar";
-            this.btnFechar.Size = new System.Drawing.Size(26, 24);
-            this.btnFechar.TabIndex = 36;
-            this.btnFechar.UseVisualStyleBackColor = false;
-            this.btnFechar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnFechar_MouseClick);
+            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox3.BackColor = System.Drawing.Color.Coral;
+            this.pictureBox3.Location = new System.Drawing.Point(201, 271);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(750, 9);
+            this.pictureBox3.TabIndex = 224;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.Coral;
+            this.pictureBox1.Location = new System.Drawing.Point(201, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1069, 10);
+            this.pictureBox1.TabIndex = 223;
+            this.pictureBox1.TabStop = false;
+            // 
+            // picimg_cliente
+            // 
+            this.picimg_cliente.BackColor = System.Drawing.Color.Transparent;
+            this.picimg_cliente.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picimg_cliente.Image = ((System.Drawing.Image)(resources.GetObject("picimg_cliente.Image")));
+            this.picimg_cliente.Location = new System.Drawing.Point(40, 24);
+            this.picimg_cliente.Name = "picimg_cliente";
+            this.picimg_cliente.Size = new System.Drawing.Size(128, 129);
+            this.picimg_cliente.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picimg_cliente.TabIndex = 221;
+            this.picimg_cliente.TabStop = false;
+            // 
+            // label18
+            // 
+            this.label18.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label18.AutoSize = true;
+            this.label18.BackColor = System.Drawing.Color.Transparent;
+            this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.ForeColor = System.Drawing.Color.Coral;
+            this.label18.Location = new System.Drawing.Point(202, 77);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(214, 26);
+            this.label18.TabIndex = 213;
+            this.label18.Text = "Dados da do Serviço";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(201, 286);
+            this.textBox1.MaxLength = 40;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(750, 389);
+            this.textBox1.TabIndex = 212;
+            // 
+            // lbldescrição
+            // 
+            this.lbldescrição.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbldescrição.AutoSize = true;
+            this.lbldescrição.BackColor = System.Drawing.Color.Transparent;
+            this.lbldescrição.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldescrição.ForeColor = System.Drawing.Color.Coral;
+            this.lbldescrição.Location = new System.Drawing.Point(202, 242);
+            this.lbldescrição.Name = "lbldescrição";
+            this.lbldescrição.Size = new System.Drawing.Size(109, 26);
+            this.lbldescrição.TabIndex = 211;
+            this.lbldescrição.Text = "Descrição";
+            // 
+            // lblinscestadual
+            // 
+            this.lblinscestadual.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblinscestadual.AutoSize = true;
+            this.lblinscestadual.BackColor = System.Drawing.Color.Transparent;
+            this.lblinscestadual.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblinscestadual.Location = new System.Drawing.Point(200, 189);
+            this.lblinscestadual.Name = "lblinscestadual";
+            this.lblinscestadual.Size = new System.Drawing.Size(98, 20);
+            this.lblinscestadual.TabIndex = 205;
+            this.lblinscestadual.Text = "Data inicial";
+            // 
+            // lblfantasia
+            // 
+            this.lblfantasia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblfantasia.AutoSize = true;
+            this.lblfantasia.BackColor = System.Drawing.Color.Transparent;
+            this.lblfantasia.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfantasia.Location = new System.Drawing.Point(559, 189);
+            this.lblfantasia.Name = "lblfantasia";
+            this.lblfantasia.Size = new System.Drawing.Size(117, 20);
+            this.lblfantasia.TabIndex = 202;
+            this.lblfantasia.Text = "Data Entrega";
+            // 
+            // Btn_Apagar
+            // 
+            this.Btn_Apagar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Btn_Apagar.BackColor = System.Drawing.Color.Salmon;
+            this.Btn_Apagar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_Apagar.Location = new System.Drawing.Point(562, 1102);
+            this.Btn_Apagar.Name = "Btn_Apagar";
+            this.Btn_Apagar.Size = new System.Drawing.Size(172, 38);
+            this.Btn_Apagar.TabIndex = 200;
+            this.Btn_Apagar.Text = "Apagar";
+            this.Btn_Apagar.UseVisualStyleBackColor = false;
+            // 
+            // Btn_Gravar
+            // 
+            this.Btn_Gravar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Btn_Gravar.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.Btn_Gravar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_Gravar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Btn_Gravar.Location = new System.Drawing.Point(207, 683);
+            this.Btn_Gravar.Name = "Btn_Gravar";
+            this.Btn_Gravar.Size = new System.Drawing.Size(172, 38);
+            this.Btn_Gravar.TabIndex = 192;
+            this.Btn_Gravar.Text = "Gravar";
+            this.Btn_Gravar.UseVisualStyleBackColor = false;
+            // 
+            // lblfuncionários
+            // 
+            this.lblfuncionários.AutoSize = true;
+            this.lblfuncionários.BackColor = System.Drawing.Color.Coral;
+            this.lblfuncionários.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfuncionários.ForeColor = System.Drawing.Color.White;
+            this.lblfuncionários.Location = new System.Drawing.Point(214, 24);
+            this.lblfuncionários.Name = "lblfuncionários";
+            this.lblfuncionários.Size = new System.Drawing.Size(449, 36);
+            this.lblfuncionários.TabIndex = 191;
+            this.lblfuncionários.Text = "Cadastro de Ordem de Serviço";
+            // 
+            // lblCódigo
+            // 
+            this.lblCódigo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblCódigo.AutoSize = true;
+            this.lblCódigo.BackColor = System.Drawing.Color.Transparent;
+            this.lblCódigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCódigo.Location = new System.Drawing.Point(200, 147);
+            this.lblCódigo.Name = "lblCódigo";
+            this.lblCódigo.Size = new System.Drawing.Size(65, 20);
+            this.lblCódigo.TabIndex = 190;
+            this.lblCódigo.Text = "Código";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(1245, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 37);
+            this.button1.TabIndex = 189;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_Codigo
+            // 
+            this.txt_Codigo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_Codigo.Location = new System.Drawing.Point(312, 144);
+            this.txt_Codigo.Name = "txt_Codigo";
+            this.txt_Codigo.Size = new System.Drawing.Size(114, 26);
+            this.txt_Codigo.TabIndex = 193;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.BackColor = System.Drawing.Color.Coral;
+            this.pictureBox2.Location = new System.Drawing.Point(2, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1299, 69);
+            this.pictureBox2.TabIndex = 222;
+            this.pictureBox2.TabStop = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dateTimePicker1.Location = new System.Drawing.Point(312, 181);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 226;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dateTimePicker2.Location = new System.Drawing.Point(694, 181);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker2.TabIndex = 227;
             // 
             // frmCadastroOrdemServico
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(976, 515);
-            this.Controls.Add(this.btnFechar);
+            this.ClientSize = new System.Drawing.Size(1300, 780);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picimg_cliente);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbldescrição);
+            this.Controls.Add(this.lblinscestadual);
+            this.Controls.Add(this.lblfantasia);
+            this.Controls.Add(this.Btn_Apagar);
+            this.Controls.Add(this.Btn_Gravar);
+            this.Controls.Add(this.lblfuncionários);
+            this.Controls.Add(this.lblCódigo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_Codigo);
+            this.Controls.Add(this.pictureBox2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmCadastroOrdemServico";
             this.Text = "frmCadastroOrdemServico";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picimg_cliente)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picimg_cliente;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbldescrição;
+        private System.Windows.Forms.Label lblinscestadual;
+        private System.Windows.Forms.Label lblfantasia;
+        private System.Windows.Forms.Button Btn_Apagar;
+        private System.Windows.Forms.Button Btn_Gravar;
+        private System.Windows.Forms.Label lblfuncionários;
+        private System.Windows.Forms.Label lblCódigo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_Codigo;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
     }
 }
